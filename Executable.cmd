be1ebc189dc3b4632b56f11caca6946f754f4ec8
medoux <merouane.mehdi@gmail.com>
2016-01-20 16:22:24 +0100

    Create Executable.cmd

---
+++ Executable.cmd
@@ -0,0 +1,16 @@
+"C:\Program Files\scilab-5.5.2\bin\WScilex.exe" -f main.sci
+
+pdfLatex AffichageLaTexDDNG.tex
+pdfLatex AffichageLaTexDDG.tex
+pdfLatex AffichageLaTexDCNG.tex
+pdfLatex AffichageLaTexDCG.tex
+
+"C:\Program Files (x86)\Adobe\Reader 11.0\Reader\AcroRd32.exe" "AffichageLaTexDDNG.pdf"
+
+"C:\Program Files (x86)\Adobe\Reader 11.0\Reader\AcroRd32.exe" "AffichageLaTexDDG.pdf"
+
+"C:\Program Files (x86)\Adobe\Reader 11.0\Reader\AcroRd32.exe" "AffichageLaTexDCNG.pdf"
+
+"C:\Program Files (x86)\Adobe\Reader 11.0\Reader\AcroRd32.exe" "AffichageLaTexDCG.pdf"
+
+
