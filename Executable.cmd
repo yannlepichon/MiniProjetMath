e0ea0c0900ae17cc1d5a9ce30c7dab2cdbbe8b48
yannlepichon <yann.lepichon.ylp@gmail.com>
2016-01-24 16:35:09 +0100

    Update Executable.cmd

--- Executable.cmd
+++ Executable.cmd
@@ -4,13 +4,3 @@ pdfLatex AffichageLaTexDDNG.tex
 pdfLatex AffichageLaTexDDG.tex
 pdfLatex AffichageLaTexDCNG.tex
 pdfLatex AffichageLaTexDCG.tex
-
-"C:\Program Files (x86)\Adobe\Reader 11.0\Reader\AcroRd32.exe" "AffichageLaTexDDNG.pdf"
-
-"C:\Program Files (x86)\Adobe\Reader 11.0\Reader\AcroRd32.exe" "AffichageLaTexDDG.pdf"
-
-"C:\Program Files (x86)\Adobe\Reader 11.0\Reader\AcroRd32.exe" "AffichageLaTexDCNG.pdf"
-
-"C:\Program Files (x86)\Adobe\Reader 11.0\Reader\AcroRd32.exe" "AffichageLaTexDCG.pdf"
-
-
