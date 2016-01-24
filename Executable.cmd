8b75e0e46cee5e1bb2967b4bf13c8367e35950ef
yannlepichon <yann.lepichon.ylp@gmail.com>
2016-01-24 16:45:40 +0100

    Update Executable.cmd

--- Executable.cmd
+++ Executable.cmd
@@ -1,6 +1,18 @@
 "C:\Program Files\scilab-5.5.2\bin\WScilex.exe" -f main.sci
 
 pdfLatex AffichageLaTexDDNG.tex
+pdfLatex AffichageLaTexDDNG.tex
+pdfLatex AffichageLaTexDDG.tex
 pdfLatex AffichageLaTexDDG.tex
 pdfLatex AffichageLaTexDCNG.tex
+pdfLatex AffichageLaTexDCNG.tex
 pdfLatex AffichageLaTexDCG.tex
+pdfLatex AffichageLaTexDCG.tex
+
+del /s .\*.toc
+del /s .\*.aux
+del /s .\*.log
+del /s .\figure1.pdf
+del /s .\figure2.pdf
+del /s .\figure3.pdf
+del /s .\figure4.pdf
